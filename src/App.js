0c531dc2e6397eaa52ce71b5bc87396c36cb99d2
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-04-06 19:31:03 -0500

    Adding Searching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,25 +8,55 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstname:'Hanad', lastname: 'Ali'},
-      company: 'walmart'
+      monsters:[],
+      searchField: ''
     }
+    console.log('constructor');
+  };
+
+  componentDidMount(){
+      console.log('componentDidMount')
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() =>{
+        return {monsters: users}
+      },
+      () => {
+        console.log(this.state);
+    }
+      ));
   }
 
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLowerCase();
+    this.setState(()=> {
+      return {searchField};
+    })
+  }
 
   render() {
+    console.log('render')
+
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster) =>{
+      return monster.name.toLowerCase().includes(searchField);
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi {this.state.name.firstname} {this.state.name.lastname}, I work at {this.state.company}
-        </p>
-        <button onClick={() => {
-          this.setState({name:{firstname: 'Bashir', lastname: 'Bob'}});
-          console.log(this.state);
-        }}>Change Name</button>
-      </header>
+        <input 
+        className='search-box' 
+        type='search' 
+        placeholder='Search monsters'
+        onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        })
+      }
     </div>
   );
 }
