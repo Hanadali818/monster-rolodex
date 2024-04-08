09142d6b46e6570462ddeaf43fcd45d8d955a69e
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-04-08 01:56:11 -0500

    created card list component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return <div>
+        <h1>Hello I am the CardList Component</h1>
+         </div>
+    }
+}
+
+
+export default CardList;
