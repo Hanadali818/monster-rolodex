8a17713afe93d50d472cf9925320641cb327c9fa
Hanadali818 <104621175+Hanadali818@users.noreply.github.com>
2024-04-24 20:55:17 -0500

    Converted the rest of my components from class to functional

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -1,9 +1,8 @@
-import { Component } from "react";
 import './card-styles.css'
 
-class Card extends Component{
-    render(){
-    const {id, name, email} = this.props.monster;
+const Card = ({monster}) => {
+    const {id, name, email} = monster;
+
         return(
             <div className="card-container" key='monster.id'>
             <img alt={`monster ${name}`} 
@@ -13,9 +12,5 @@ class Card extends Component{
         <p>{email}</p>
         </div>
     )
-    
-        
-    }
-
 }
 export default Card
